432bc780b239763968249197bddbe96308ad32b5
Charles Osuya <charles@arkmarketplace.com>
2025-08-20 19:13:39 +0100

    wip

---
+++ app/user.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import ProfileCard from "@/components/ui/profile-card";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+function useDebounce(value: any, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const repsonse = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(repsonse);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
+const User = () => {
+  const [response, setResponse] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setloading] = useState(true);
+
+  const debouncedSearch = useDebounce(search, 300);
+
+  const fetachUsers = async () => {
+    try {
+      const request = await axios.get("https://api.github.com/users");
+      setResponse(request.data);
+    } catch (error) {
+    } finally {
+      setloading(false);
+    }
+  };
+  useEffect(() => {
+    fetachUsers();
+  }, []);
+
+  const filteredUsers = () => {
+    if (!debouncedSearch) return response;
+
+    return response.filter((user: any) =>
+      user?.login?.toLowerCase().includes(debouncedSearch.toLowerCase())
+    );
+  };
+
+  if (loading) return <div>loading....</div>;
+
+  return (
+    <main className=" max-w-5xl mx-auto">
+      <div className="mt-20">
+        <Input value={search} onChange={(e) => setSearch(e.target.value)} />
+        {/* <Button>Search</Button> */}
+      </div>
+
+      <div className="grid md:grid-cols-3 gap-4 mt-20">
+        {filteredUsers().map((user: any) => (
+          <div key={user.id}>
+            <Link href={`/users/${user.login}`}>
+              <ProfileCard
+                username={user.login}
+                image={user.avatar_url}
+                link={user.url}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default User;
---
+++ app/users/[username]/page.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import axios from "axios";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useCharacterLimit } from "@/hooks/use-character-limit";
+import { useImageUpload } from "@/hooks/use-image-upload";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ImagePlus } from "lucide-react";
+import { useId } from "react";
+
+export default function ExampleClientComponent() {
+  const params = useParams();
+  const username = params.username;
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({
+    name: "",
+    avatar_url: "",
+    location: "",
+    followers: "",
+    following: "",
+  });
+
+  useEffect(() => {
+    if (!username) return;
+
+    const fetchUserData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `https://api.github.com/users/${username}`
+        );
+        console.log(response.data, "loki");
+        setUser(response.data);
+        return;
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUserData();
+  }, [username]);
+
+  if (loading) return <div>loading....</div>;
+
+  if (user)
+    return (
+      // <div className="  border shadow gap-4 max-w-5xl mx-auto text-3xl">
+      //   <div>Name: {user && user?.name}</div>
+      //   <div>Bio: {user.bio ?? "N/a"}</div>
+      //   <div>Location: {user.location} </div>
+      //   <div>Followers: {user.followers} </div>
+      //   <div>Following: {user.following}</div>
+      // </div>
+
+      <div>
+        <Dialog open>
+          <DialogContent className="sm:max-w-xl p-0 overflow-hidden rounded-2xl border">
+            <div
+              className="px-6 py-4 h-36"
+              style={{
+                background:
+                  "radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(148, 187, 233, 1) 100%)",
+              }}
+            />
+
+            <div className="-mt-14 flex justify-center">
+              <div className="relative">
+                <Avatar className="h-24 w-24 border-4 border-background shadow-lg rounded-full">
+                  <AvatarImage src={user.avatar_url} alt="Profile" />
+                  <AvatarFallback>U</AvatarFallback>
+                </Avatar>
+              </div>
+            </div>
+
+            <div className="max-h-[50vh] overflow-y-auto px-6 py-6 space-y-4">
+              <form className="space-y-4">
+                <div className="flex flex-col sm:flex-row gap-4">
+                  <div className="flex-1 space-y-1.5">
+                    <Label>Name</Label>
+                    <Input
+                      placeholder="E.g. John Doe"
+                      defaultValue={user.name}
+                    />
+                  </div>
+                  <div className="flex-1 space-y-1.5">
+                    <Label>Location</Label>
+                    <Input
+                      placeholder="Frontend Developer"
+                      defaultValue={user.location}
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row gap-4">
+                  <div className="flex-1 space-y-1.5">
+                    <Label>Bio</Label>
+                    <Input type="email" defaultValue="margaret@email.com" />
+                  </div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row gap-4">
+                  <div className="flex-1 space-y-1.5">
+                    <Label>Folllowers</Label>
+                    <Input defaultValue={user.followers} />
+                  </div>
+                  <div className="flex-1 space-y-1.5">
+                    <Label>Following</Label>
+                    <Input defaultValue={user.following} />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+    );
+}
